be7fd8a9694f40f5bee50d66f809c9812f47f971
pranav-dev1 <pranavs.dev01@gmail.com>
2026-03-18 03:16:40 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,6 @@
+const accountId="144553"
+let accountEmail="hitesh@gmail.com"
+var accountPassword="12345"
+accountCity="Jaipur"
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity]);
