58b421ab3789de6792174a3d5aabbb253151ce12
pranav-dev1 <pranavs.dev01@gmail.com>
2026-03-18 03:17:22 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,18 @@
+"use strict";  //treat all js code as newer version
+//alert(3+3)     we are using nodejs not browser
+let name="Pranav";
+let age=39;
+let isLoggedIn=false;
+
+//----Primitive data types------
+//number-> 2 to power 53
+//bigint
+//string-> ""
+//boolean-> true/false
+//null-> standalone value
+//undefined->
+//symbol-> unique
+
+//object
+console.log(typeof undefined);
+console.log(typeof null);
